11f321be1f31920311eb41136019c9e46077e77a
Elsti97 <elsti.shehati@hotmail.it>
2023-03-22 15:00:43 +0100

    Bonus done

--- CalcoliHelper.cs
+++ CalcoliHelper.cs
@@ -5,28 +5,61 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
+
+    //Polimorfismo, più metodi definiti con lo stesso nome, numero diverso di parametri
     public class CalcoliHelper
     {
-        public static int sommaInt(int a, int b)=>(a + b);
-        public static double sommaDouble(double a, double b)=>(a + b);
+        public static int somma(int a, int b)=>(a + b);
+        public static double somma(double a, double b)=>(a + b);
 
-        public static int diffInt(int a, int b) => (a - b);
-        public static double diffDouble(double a, double b) => (a - b);
+        public static int diff(int a, int b) => (a - b);
+        public static double diff(double a, double b) => (a - b);
 
-        public static int moltInt(int a, int b) => (a * b);
-        public static double moltDouble(double a, double b) => (a * b);
+        public static int molt(int a, int b) => (a * b);
+        public static double molt(double a, double b) => (a * b);
 
-        public static int divInt(int a, int b)=> (a / b);
-        public static double divDouble(double a, double b) => (a / b);
+        public static int div(int a, int b)=> (a / b);
+        public static double div(double a, double b) => (a / b);
 
-        public static int absInt(int a)=>(Math.Abs(a));
-        public static double absDouble(double a) =>(Math.Abs(a));
+        public static int abs(int a)=>(Math.Abs(a));
+        public static double abs(double a) =>(Math.Abs(a));
 
-        public static int minInt(int a, int b)=>(Math.Min(a, b));
-        public static double minDouble(double a, double b)=>(Math.Min(a, b));
+        public static int min(int a, int b)=>(Math.Min(a, b));
+        public static double min(double a, double b)=>(Math.Min(a, b));
 
-        public static int maxInt(int a, int b) => (Math.Max(a, b));
-        public static double maxDouble(double a, double b) => (Math.Max(a, b));
+        public static int max(int a, int b) => (Math.Max(a, b));
+        public static double max(double a, double b) => (Math.Max(a, b));
+
+
+    //BONUS
+    public static double potenza(int a, int b)
+    {
+        if (a == 0 && b == 0)
+        {
+            return 1;
+        }
+        else if (b == 0)
+        {
+            return 1;
+        }
+        else if (a == 0)
+        {
+            return 0;
+        }
+        else if (b < 0)
+        {
+            return 1 / potenza(a, -b);
+        }
+        else
+        {
+            int risultato = a;
+            for (int i = 1; i < b; i++)
+            {
+                risultato *= a;
+            }
+            return risultato;
+        }
     }
+}
 
 
--- Program.cs
+++ Program.cs
@@ -3,25 +3,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Somma singola: " + CalcoliHelper.sommaInt(1, 2));
-        Console.WriteLine("Somma Doppia: " + CalcoliHelper.sommaDouble(1.5, 3.6));
+        Console.WriteLine("Somma singola: " + CalcoliHelper.somma(1, 2));
+        Console.WriteLine("Somma Doppia: " + CalcoliHelper.somma(1.5, 3.6));
 
-        Console.WriteLine("Differenza singola: " + CalcoliHelper.diffInt(5, 3));
-        Console.WriteLine("Differenza Doppia: " + CalcoliHelper.diffDouble(5.5, 3.5));
+        Console.WriteLine("Differenza singola: " + CalcoliHelper.diff(5, 3));
+        Console.WriteLine("Differenza Doppia: " + CalcoliHelper.diff(5.5, 3.1));
 
-        Console.WriteLine("Moltiplicazione singola: " + CalcoliHelper.moltInt(2, 3));
-        Console.WriteLine("Moltiplicazione Doppia: " + CalcoliHelper.moltDouble(2.5, 3.5));
+        Console.WriteLine("Moltiplicazione singola: " + CalcoliHelper.molt(2, 3));
+        Console.WriteLine("Moltiplicazione Doppia: " + CalcoliHelper.molt(2.5, 3.5));
 
-        Console.WriteLine("Divisione singola: " + CalcoliHelper.divInt(6, 3));
-        Console.WriteLine("Divisione Doppia: " + CalcoliHelper.divDouble(10.5, 0.1));
+        Console.WriteLine("Divisione singola: " + CalcoliHelper.div(6, 3));
+        Console.WriteLine("Divisione Doppia: " + CalcoliHelper.div(10.5, 0.2));
 
-        Console.WriteLine("Assoluto singolo: " + CalcoliHelper.absInt(-10));
-        Console.WriteLine("Assoluto Doppio: " + CalcoliHelper.absDouble(-10.5));
+        Console.WriteLine("Assoluto singolo: " + CalcoliHelper.abs(-10));
+        Console.WriteLine("Assoluto Doppio: " + CalcoliHelper.abs(-10.5));
 
-        Console.WriteLine("Minimo singolo: " + CalcoliHelper.minInt(2, 3));
-        Console.WriteLine("Minimo Doppio: " + CalcoliHelper.minDouble(2.5, 3.5));
+        Console.WriteLine("Minimo singolo: " + CalcoliHelper.min(2, 3));
+        Console.WriteLine("Minimo Doppio: " + CalcoliHelper.min(2.5, 3.5));
 
-        Console.WriteLine("Massimo singolo: " + CalcoliHelper.maxInt(2, 3));
-        Console.WriteLine("Massimo Doppio: " + CalcoliHelper.maxDouble(2.5, 3.5));
+        Console.WriteLine("Massimo singolo: " + CalcoliHelper.max(2, 3));
+        Console.WriteLine("Massimo Doppio: " + CalcoliHelper.max(2.5, 3.5));
+
+        Console.WriteLine("Potenza: " + CalcoliHelper.potenza(5, 1));
     }
 }
