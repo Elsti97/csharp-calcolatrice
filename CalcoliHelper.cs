aee2faef54f8cfec5571782eb0310f47df8ac36c
Elsti97 <elsti.shehati@hotmail.it>
2023-03-22 14:35:58 +0100

    Basic exercise done

---
+++ CalcoliHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class CalcoliHelper
+    {
+        public static int sommaInt(int a, int b)=>(a + b);
+        public static double sommaDouble(double a, double b)=>(a + b);
+
+        public static int diffInt(int a, int b) => (a - b);
+        public static double diffDouble(double a, double b) => (a - b);
+
+        public static int moltInt(int a, int b) => (a * b);
+        public static double moltDouble(double a, double b) => (a * b);
+
+        public static int divInt(int a, int b)=> (a / b);
+        public static double divDouble(double a, double b) => (a / b);
+
+        public static int absInt(int a)=>(Math.Abs(a));
+        public static double absDouble(double a) =>(Math.Abs(a));
+
+        public static int minInt(int a, int b)=>(Math.Min(a, b));
+        public static double minDouble(double a, double b)=>(Math.Min(a, b));
+
+        public static int maxInt(int a, int b) => (Math.Max(a, b));
+        public static double maxDouble(double a, double b) => (Math.Max(a, b));
+    }
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Somma singola: " + CalcoliHelper.sommaInt(1, 2));
+        Console.WriteLine("Somma Doppia: " + CalcoliHelper.sommaDouble(1.5, 3.6));
+
+        Console.WriteLine("Differenza singola: " + CalcoliHelper.diffInt(5, 3));
+        Console.WriteLine("Differenza Doppia: " + CalcoliHelper.diffDouble(5.5, 3.5));
+
+        Console.WriteLine("Moltiplicazione singola: " + CalcoliHelper.moltInt(2, 3));
+        Console.WriteLine("Moltiplicazione Doppia: " + CalcoliHelper.moltDouble(2.5, 3.5));
+
+        Console.WriteLine("Divisione singola: " + CalcoliHelper.divInt(6, 3));
+        Console.WriteLine("Divisione Doppia: " + CalcoliHelper.divDouble(10.5, 0.1));
+
+        Console.WriteLine("Assoluto singolo: " + CalcoliHelper.absInt(-10));
+        Console.WriteLine("Assoluto Doppio: " + CalcoliHelper.absDouble(-10.5));
+
+        Console.WriteLine("Minimo singolo: " + CalcoliHelper.minInt(2, 3));
+        Console.WriteLine("Minimo Doppio: " + CalcoliHelper.minDouble(2.5, 3.5));
+
+        Console.WriteLine("Massimo singolo: " + CalcoliHelper.maxInt(2, 3));
+        Console.WriteLine("Massimo Doppio: " + CalcoliHelper.maxDouble(2.5, 3.5));
+    }
+}
